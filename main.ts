762a8bbf91d0cbc97347c1a775ae2a0ae9bf6c95
kanejun06 <60838183+kanejun06@users.noreply.github.com>
2020-09-04 17:18:10 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,141 @@
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    teki.destroy(effects.disintegrate, 100)
+    music.pewPew.play()
+    info.changeScoreBy(1)
+    enemyFlag = 0
+})
+info.onCountdownEnd(function () {
+    game.over(true, effects.confetti)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (controller.dx() != 0 || controller.dy() != 0) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . c b a c . . . . . . 
+            . . . . c c b c f a c . . . . . 
+            . . . . a f b b b a c . . . . . 
+            . . . . a f f b a f c c . . . . 
+            . . . . c b b a f f c . . . . . 
+            . . . . . b b a f a . . . . . . 
+            . . . . . . c b b . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySelf, controller.dx(100) * shooterScale, controller.dy(100) * shooterScale)
+    }
+})
+let enemyY = 0
+let enemyX = 0
+let posX = 0
+let posY = 0
+let projectile: Sprite = null
+let enemyFlag = 0
+let teki: Sprite = null
+let shooterScale = 0
+let mySelf: Sprite = null
+mySelf = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . b 5 5 b . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . b b b b b 5 5 5 5 5 5 5 b . . 
+    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b d 5 5 b 1 f f 5 4 4 c . . 
+    b b d b 5 5 5 d f b 4 4 4 4 b . 
+    b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySelf)
+mySelf.setFlag(SpriteFlag.StayInScreen, true)
+shooterScale = 50
+info.startCountdown(60)
+forever(function () {
+    mySelf.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . b b b b b 5 5 5 5 5 5 5 b . . 
+        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+        . . b d 5 5 b 1 f f 5 4 4 c . . 
+        b b d b 5 5 5 d f b 4 4 4 4 4 b 
+        b d d c d 5 5 b 5 4 4 4 4 4 b . 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `)
+    pause(100)
+    mySelf.setImage(img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . . . . b c . . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 5 d f . . 
+        . . . . b 5 5 1 f f 5 d 4 c . . 
+        . . . . b 5 5 d f b d d 4 4 . . 
+        b d d d b b d 5 5 5 4 4 4 4 4 b 
+        b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+        b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+        c d d c d 5 5 b 5 5 5 5 5 5 b . 
+        c b d d c c b 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `)
+    pause(100)
+    console.logValue("x", controller.dx(100) * 10)
+    console.logValue("y", controller.dy(100) * 10)
+    posY = randint(0, 120)
+    posX = randint(0, 160)
+    if (!(posX > 10 && posX < 150 && (posY > 10 && posY < 110))) {
+        enemyX = posX
+        enemyY = posY
+    }
+    if (enemyFlag == 0) {
+        enemyFlag = 1
+        teki = sprites.create(img`
+            ........................
+            ........................
+            ........................
+            ........................
+            ..........ffff..........
+            ........ff1111ff........
+            .......fb111111bf.......
+            .......f11111111f.......
+            ......fd11111111df......
+            ......fd11111111df......
+            ......fddd1111dddf......
+            ......fbdbfddfbdbf......
+            ......fcdcf11fcdcf......
+            .......fb111111bf.......
+            ......fffcdb1bdffff.....
+            ....fc111cbfbfc111cf....
+            ....f1b1b1ffff1b1b1f....
+            ....fbfbffffffbfbfbf....
+            .........ffffff.........
+            ...........fff..........
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Enemy)
+        teki.setPosition(enemyX, enemyY)
+        teki.follow(mySelf, randint(10, 50))
+    }
+})
